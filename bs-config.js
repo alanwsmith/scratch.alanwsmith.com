805eac8fd364fa3fd7c397503816fd217dcaae6f
Alan Smith <commit@alanwsmith.com>
2022-10-30 01:07:31 -0400

    Initil commit

---
+++ bs-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    browser: ['safari'],
+    injectChanges: false,
+    ghostMode: false,
+    minify: false,
+    notify: false,
+    port: 3000,
+    reloadOnRestart: true,
+    server: {
+        baseDir: 'site',
+    },
+    watch: true,
+}
